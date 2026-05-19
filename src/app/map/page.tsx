b5fca6ce4ae0f7f1443e4cd610ff6eb7d37ea4d2
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-20 00:28:02 +0900

    ヘッダーの作成。途中まで完了

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,3 +1,31 @@
-export default function Mapp1(){
-  return <h1>よろ</h1>
+import { MapList } from "@/components/Maplist"
+import Link from "next/link"
+import Image from "next/image"
+
+export default async function Mapp1() {
+  return (
+    <>
+      <div className="flex justify-between items-center text-black  pt-10 mb-5 border-b">
+        <div className="flex">
+          <Image
+          src="/VALG.png"
+          alt="VALOGO"
+          width={30}
+          height={30}
+          />
+          <h1 className="text-black">RANT BEGINNER</h1>
+        </div>
+        <div>
+        <Link href="/" className="text-black">HOME</Link>
+        <Link href="/guns" className="text-black">WEAPON</Link>
+        <Link href="/map" className="text-black">MAP</Link>
+        </div>
+      </div>
+      <MapList />
+    </>
+  )
 }
+
+
+
+
