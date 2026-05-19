7858495479d21e699734332209deec0aca0c8244
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-20 00:28:41 +0900

    背景画像の挿入など

--- src/components/Midashi.tsx
+++ src/components/Midashi.tsx
@@ -5,21 +5,21 @@ import Link from "next/link"
 
 export const Midashi = ({ midashi, src, honbun, page }: midashiProps) => {
   return (
-    <div className="flex mt-20">
+    <div className="flex mt-20 rouded-2xl backdrop-blur-sm mb-20 bg-black/30 shadow-2xs ">
       <Link href={page}>
         <Image
           src={src}
           alt={midashi}
           width={600}
           height={600}
-          className="mr-40 rounded-2xl"
+          className="mr-40 rounded-2xl border "
         />
         </Link>
-      <div>
-        <h1 className="text-white text-8xl mb-20 ">{midashi}</h1>
-        <p className="text-white font-bold">{honbun}</p>
+      <div className="flex flex-col justify-center ">
+        <h1 className="text-white text-8xl mb-10 text-shadow-2xs ">{midashi}</h1>
+        <div className="bg-white h-[2px] w-[1000px]" />
+        <p className="text-white font-bold mt-10">{honbun}</p>
       </div>
-
     </div>
   )
 }
