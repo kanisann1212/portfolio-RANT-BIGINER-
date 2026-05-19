c7f74f5218a616729d8fede73cf8d91368fd72be
yuuma <obakasann1819@gmail.com>
2026-05-19 17:25:17 +0900

    誤字の修正

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,7 +1,7 @@
 export const Top = () => {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h1 className='font-bold text-white italic text-center text-9xl ' >RANT BIGINER</h1>
+        <h1 className='font-bold text-white italic text-center text-9xl ' >RANT BEGINNER</h1>
       </div>
   )
 }
