1e5f90ef32d576df5fe47b7a4caa16ba67dba832
yuuma <obakasann1819@gmail.com>
2026-05-19 13:27:29 +0900

    リファクタリングと見栄えの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
 import { User } from "lucide-react"
+import { Video } from "@/components/Video";
+import { Top } from "@/components/Top";
 export default function Home() {
   return (
     <>
-      <div className="relative h-screen blur-xs">
-        <video
-          className="inset-0 w-full h-full object-cover "
-          src="/videos/_1_1472600621151420416.mp4"
-          autoPlay
-          loop
-          muted
-          playsInline
-        />
+      <Video />
+      <div className="absolute top-10 right-10" >
+        <User className="w-7 h-7 text-white" />
       </div>
-      <span className="absolute bg-white rounded-4xl w-10 h-10 opacity-70">
-        <User className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
-      </span>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h1 className='font-bold text-white italic text-center text-9xl ' >RANT BIGINER</h1>
-        <p className="text-center text-2xl text-white">ーVALORANT初心者のためのサイトー</p>
-        <button className="text-center bg-blue-600 text-red-600 rounded-lg w-15 h-10 mix-blend-screen">始める</button>
-      </div>
+      <Top />
     </>
   );
 }
---
+++ src/components/Top.tsx
@@ -0,0 +1,10 @@
+export const Top = () => {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="flex items-center justify-center flex-col ">
+        <h1 className='font-bold text-white italic text-center text-9xl ' >RANT BIGINER</h1>
+        <button className="text-center text-white border-solid-white rounded-lg w-15 h-10 m-15mix-blend-screen object-center">START</button>
+      </div>
+    </div>
+  )
+}
