3d7cf1a7f57c11453a50457134358f8f60ca2207
yuuma <obakasann1819@gmail.com>
2026-05-19 17:25:34 +0900

    型定義の修正

--- src/components/Midashi.tsx
+++ src/components/Midashi.tsx
@@ -1,20 +1,25 @@
 import type { midashiProps } from "@/types/type"
 import Image from "next/image"
+import Link from "next/link"
 
-export const Midashi = ({midashi,src}:midashiProps) => {
+
+export const Midashi = ({ midashi, src, honbun, page }: midashiProps) => {
   return (
-            <div className="flex">
-              <Image
-                src={src}
-                alt={midashi}
-                width={600}
-                height={600}
-                className="mr-40"
-              />
-              <div>
-                <h1 className="text-white text-8xl mb-20 ">{midashi}</h1>
-                <p>マップ構造を学び、戦略に生かす</p>
-              </div>
-            </div>
+    <div className="flex mt-20">
+      <Link href={page}>
+        <Image
+          src={src}
+          alt={midashi}
+          width={600}
+          height={600}
+          className="mr-40 rounded-2xl"
+        />
+        </Link>
+      <div>
+        <h1 className="text-white text-8xl mb-20 ">{midashi}</h1>
+        <p className="text-white font-bold">{honbun}</p>
+      </div>
+
+    </div>
   )
 }
