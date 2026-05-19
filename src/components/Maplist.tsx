76b3ea4547957f0f8299ef20a154682122c09d68
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-20 08:19:34 +0900

    マップリストの修正、スタイル

--- src/components/Maplist.tsx
+++ src/components/Maplist.tsx
@@ -11,18 +11,18 @@ export const MapList = async () => {
     .filter((map: MapType) => map.tacticalDescription)
 
   return (
-    <div>
+    <div className="grid grid-cols-3 gap-2 m-5">
       {BattleMap.map((item) => {
         return (
-          <div key={item.uuid} className="flex">
+          <div key={item.uuid} className="relative rounded-3xl" >
             <Image
               src={item.splash}
               alt={item.displayName}
-              width={400}
-              height={400}
-              className="h-auto" />
-            <h1 className="text-black">{item.displayName}</h1>
-          </div>
+              width={900}
+              height={338}
+             />
+            <h1 className="text-white text-3xl absolute top-4 left-4">{item.displayName}</h1>
+            </div>
         )
       })}
     </div>
