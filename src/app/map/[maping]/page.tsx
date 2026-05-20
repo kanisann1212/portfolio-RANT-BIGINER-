9b456121a7f93cfd8b0c930f88b4e845bfa85d45
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-21 00:32:02 +0900

    リファクタリング

--- src/app/map/[maping]/page.tsx
+++ src/app/map/[maping]/page.tsx
@@ -1,43 +1,21 @@
 import { Header } from "@/components/Header"
+import { MapSChild } from "@/components/MapSChild"
 import type { MapType } from "@/types/type"
-import Image from "next/image"
+
 
 type Prop = {
   params: Promise<{ maping: string }>
 }
 
-export default async function Mapp({ params }: Prop) {
+export default async function MapS({ params }: Prop) {
   const { maping } = await params
   const res = await fetch(`https://valorant-api.com/v1/maps/${maping}`)
   const json = await res.json()
   const MapMicro: MapType = json.data
-  console.log(MapMicro)
- 
   return (
     <>
       <Header />
-      <div
-        style={{
-          backgroundImage: `url(${MapMicro.stylizedBackgroundImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          minHeight: "200px",
-        }}
-      >
-        <h1 className="text-9xl">{MapMicro.displayName}</h1>
-        <div className="flex justify-center">
-        <div className="relative w-[1000]">
-        <Image
-          src={MapMicro.displayIcon}
-          alt="VALORANT"
-          width={500}
-          height={500}
-          className="pl-auto pr-auto w-full h-auto"
-        />
-        </div>
-        </div>
-        <p>{MapMicro.tacticalDescription}</p>
-      </div>
+      <MapSChild MapMicro={MapMicro} />
     </>
   )
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,9 @@
-import { User } from "lucide-react"
-import { Video } from "@/components/Video";
 import { Top } from "@/components/Top";
-import { Midashi } from "@/components/Midashi";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Video />
-      <div className="fixed left-10 top-10" >
-        <Image 
-        src="/VALORANT.png"
-        alt="VALORANT"
-        width={200}
-        height={400}
-        />
-      </div>
-      <div className="fixed top-10 right-10" >
-        <User className="w-10 h-10 text-white" />
-      </div>
-      <Top />
-      <div className=" mt-40 bg-[image:url('/BGVAL.jpg')] bg-contain bg-center  rounded-2xl w-full  mask-t-from-95% mask-b-from-90%  ">
-        <Midashi
-          src={"/abys.webp"}
-          midashi={"MAP"}
-          honbun={"構造を学び、戦略に活かす"}
-          page={"/map"}
-        />
-        <Midashi
-          src={"/egents.avif"}
-          midashi={"EGENT"}
-          honbun={"己を知り、チームを強化する"}
-          page={"/egents"}
-        />
-        <Midashi
-          src={"/wepon.avif"}
-          midashi={"WEPON"}
-          honbun={"定められた平等の力を活かす"}
-          page={"/guns"}
-        />
-      </div>
+    <Top />
     </>
   );
 }
