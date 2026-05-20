cc2747f202efb9f153c198ad34c863926f55e03a
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-21 08:22:15 +0900

    AGENTページのデータ取得

--- src/app/egents/page.tsx
+++ src/app/egents/page.tsx
@@ -1,5 +1,15 @@
 import { Header } from "@/components/Header"
+import type { Agent } from "@/types/type"
 
-export default function  Egents()  {
-  return <Header />
+export default async function Egents() {
+  const res = await fetch("https://valorant-api.com/v1/agents?isPlayableCharacter=true&language=ja-JP")
+  const json = await res.json()
+  const Sentinel = json.data
+  .filter((EGEN:Agent)=>EGEN.role.displayName === "センチネル")
+  console.log(Sentinel)
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
