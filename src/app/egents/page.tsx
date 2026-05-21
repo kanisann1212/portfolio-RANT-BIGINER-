c4b00d94dafaba34288a5f858304bb6595476bd3
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-22 00:20:53 +0900

    各ページの見栄え作り

--- src/app/egents/page.tsx
+++ src/app/egents/page.tsx
@@ -1,5 +1,9 @@
-import { AGENT } from "@/components/AGENT/AGENT"
+import { CONT } from "@/components/AGENT/CONT"
+import { DUE } from "@/components/AGENT/DUE"
+import { INI } from "@/components/AGENT/INI"
+import { SEN } from "@/components/AGENT/SEN"
 import { Header } from "@/components/TOP PAGE/Header"
+import { MIBAE } from "@/components/TOP PAGE/MIBAE"
 import type { Agent } from "@/types/type"
 
 export default async function Egents() {
@@ -17,14 +21,14 @@ export default async function Egents() {
   return (
     <>
       <Header />
-      <h1 className="text-black text-5xl mt-24">Sentinel</h1>
-      <AGENT A= {Sentinel} />
-      <h1 className="text-black text-5xl mt-24">Duelist</h1>
-      <AGENT A= {duelist} />
-       <h1 className="text-black text-5xl mt-24">Initiator</h1>
-      <AGENT A= {Initiater} />
-       <h1 className="text-black text-5xl mt-24">Controller</h1>
-      <AGENT A= {controller} />
-      </>
+      <MIBAE syubun="ヴァロラントはチームプレイが必須です。"hukubun="キャラの理解を深めて勝利をつかみましょう" kime="ーONE FOR ALLー" gazou="/AGENTP.jpg"/>
+      <SEN A={Sentinel} />
+      <h1 className="text-black text-9xl m-10 text-center"></h1>
+      <DUE A={duelist} />
+      <h1 className="text-black text-9xl m-10  text-center"></h1>
+      <INI A={Initiater} />
+      <h1 className="text-black text-9xl m-10  text-center"></h1>
+      <CONT A={controller} />
+    </>
   )
 }
--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,10 +1,12 @@
 import { MapList } from "@/components/MAP/Maplist"
 import { Header } from "@/components/TOP PAGE/Header"
+import { MIBAE } from "@/components/TOP PAGE/MIBAE"
 
 export default async function Mapp1() {
   return (
     <>
       <Header />
+      <MIBAE syubun="ヴァロラントは頭脳戦です。" hukubun="頭を使い地の利を生かしましょう" kime="ーGame onー" gazou="/MAPT.jpg"/>
       <MapList />
     </>
   )
