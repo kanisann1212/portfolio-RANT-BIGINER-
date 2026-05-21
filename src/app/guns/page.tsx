cf83ff828f46d45c07a68749286804afb69f0e82
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-22 00:25:22 +0900

    コンソールでデータの中身を確認

--- src/app/guns/page.tsx
+++ src/app/guns/page.tsx
@@ -11,6 +11,7 @@ export default async function Guns() {
     ...wepon,
     skins: wepon.skins.filter((skin: Skined) => skin.chromas.length >= 4),
   }))
+  console.log(filterdWeponSkin)
 
   return (
     <>
@@ -21,46 +22,18 @@ export default async function Guns() {
             href={`${basepath}/${wepon.uuid}`}
             key={wepon.uuid}
             className="relative rounded-3xl"
-          // style={{
-          //   // backgroundImage: `url(${ })`,
-          //   // backgroundSize: "cover",
-          //   // backgroundPosition: "center",
-          //   // minHeight: "200px",
-          // }}
           >
             <Image src={wepon.displayIcon} alt={wepon.uuid} width={200} height={400} />
-            <p>武器名：{wepon.displayName}</p>
-            <p>発射レート：{wepon.weaponStats?.fireRate}</p>
-            <p>装填弾数：{wepon.weaponStats?.magazineSize}</p>
-            <p>リロード速度：{wepon.weaponStats?.reloadTimeSeconds}</p>
-            <p>集弾率：{wepon.weaponStats?.firstBulletAccuracy}</p>
-            <p>{wepon.shopData?.categoryText}</p>
-            <p>{wepon.shopData?.cost}</p>
-            {wepon.skins.map((skin: Skined) => (
-              <div key={skin.uuid}>
-                {skin.displayIcon && (
-                  <Image src={skin.displayIcon} alt={wepon.uuid} width={200} height={400} />
-                )}
-                <p>{skin.displayName}</p>
-                {skin.chromas.map((chroma: SkinLevels) => (
-                  <div key={chroma.uuid}>
-                    {chroma.streamedVideo && (
-                      <video
-                        className="inset-0 w-full h-full object-cover"
-                        src={chroma.streamedVideo}
-                        autoPlay
-                        loop
-                        muted
-                        playsInline
-                      />
-                    )}
-                  </div>
-                ))}
-              </div>
-            ))}
           </Link>
-        </div>
+          <p>武器名：{wepon.displayName}</p>
+          <p>発射レート：{wepon.weaponStats?.fireRate}</p>
+          <p>装填弾数：{wepon.weaponStats?.magazineSize}</p>
+          <p>リロード速度：{wepon.weaponStats?.reloadTimeSeconds}</p>
+          <p>集弾率：{wepon.weaponStats?.firstBulletAccuracy}</p>
+          <p>{wepon.shopData?.categoryText}</p>
+          <p>{wepon.shopData?.cost}</p>
 
+        </div>
       ))}
     </>
   )
