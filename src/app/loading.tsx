2aa416a162d1840a8eb7ab7f2d03e11666b6b9e4
yuuma <obakasann1819@gmail.com>
2026-05-22 17:41:40 +0900

    Loding UIの追加

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <img src="/Loading.gif" alt="Loading..." width={150} height={150} />
+      <h1 className="text-black">Loading......</h1>
+    </div>
+  )
+}
