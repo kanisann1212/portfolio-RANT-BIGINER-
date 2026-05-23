0a235a65b96ed53454b5ff157141f3c2feaefb68
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-23 20:28:43 +0900

    データの取得

--- src/app/guns/[gun]/page.tsx
+++ src/app/guns/[gun]/page.tsx
@@ -1,16 +1,21 @@
 import { Header } from "@/components/TOP PAGE/Header";
+import { VandalLayout } from "@/components/GUN LAYOUT/VandalLayout";
 
 type GunPromise = {
-  params:Promise<{gun:string}>
+  params: Promise<{ gun: string }>
 }
 
-export default async function Gun( {params}:GunPromise){
-  const {gun} = await params
-
-  return(
+export default async function Gun({ params }: GunPromise) {
+  const { gun } = await params
+  const res = await fetch(`https://valorant-api.com/v1/weapons/${gun}`)
+  const json = await res.json()
+  const Weponinfo = json.data
+  const WeponName = json.data.displayName
+  console.log(Weponinfo)
+  return (
     <>
       <Header />
+      {WeponName === "Vandal" && <VandalLayout Guninfo={Weponinfo}/>}
     </>
   )
-
 }
