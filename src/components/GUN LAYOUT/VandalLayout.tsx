7f24fcdffa6c78911e6cef401e6de936595d7a8f
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-23 20:27:24 +0900

    画像の追加

---
+++ src/components/GUN LAYOUT/VandalLayout.tsx
@@ -0,0 +1,13 @@
+import type { WeponType } from "@/types/type"
+
+type Props = { 
+  Guninfo :WeponType
+}
+
+export const VandalLayout = ({Guninfo}: Props) =>{
+  return(
+    <>
+
+    </>
+  )
+}
