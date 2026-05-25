88d3a88c3d5027fe26d567feb11843c92d30e1dc
yuuma <obakasann1819@gmail.com>
2026-05-25 15:26:26 +0900

    銃ページのレスポンシブ対応の追加

--- src/components/GUN LAYOUT/WeponSkinPattern.tsx
+++ src/components/GUN LAYOUT/WeponSkinPattern.tsx
@@ -3,10 +3,10 @@ import { Gauge } from "./gauge"
 
 type Props = {
   Guninfo: WeponType
-  Windex:number
+  Windex: number
 }
 
-export const WeponSkinPatternLayout = ({ Guninfo,Windex }: Props) => {
+export const WeponSkinPatternLayout = ({ Guninfo, Windex }: Props) => {
   const Gaia1 = Guninfo.skins[Windex]
   const Gaia2 = Gaia1.chromas[1]
   console.log(Guninfo)
@@ -23,18 +23,24 @@ export const WeponSkinPatternLayout = ({ Guninfo,Windex }: Props) => {
         />
       </div>
       <h1 className="absolute top-30 left-20 text-7xl" >{Gaia2.displayName}</h1>
-      <h1 className="absolute top-220 left-20 text-7xl" >{Guninfo.displayName}</h1>
-      <div className="absolute bottom-20 backdrop-blur-sm">
-        <Gauge value={Guninfo.weaponStats.magazineSize} max={100} label="magazinsizeー装填段数:" />
-      </div>
-      <div className="absolute bottom-20 left-125 backdrop-blur-sm">
-        <Gauge value={Guninfo.weaponStats.reloadTimeSeconds} max={5} reverse label="reloadTimeSecondsーリロード速度:" />
-      </div>
-      <div className="absolute bottom-20 left-300 backdrop-blur-sm">
-      <Gauge value={Guninfo.weaponStats.firstBulletAccuracy} max={5} label="firstBulletAccuracyー集団率:" />
-      </div>
-       <div className="absolute bottom-20 left-455 backdrop-blur-sm">
-      <Gauge value={Guninfo.weaponStats.fireRate} max={16} label="fireRateー発射レート:" />
+      <div className="absolute bottom-10">
+        <div className="flex flex-col">
+          <h1 className="text-7xl mb-10 ml-10" >{Guninfo.displayName}</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 ">
+            <div className="backdrop-blur-sm">
+              <Gauge value={Guninfo.weaponStats.magazineSize} max={100} label="magazinsizeー装填段数" />
+            </div>
+            <div className="backdrop-blur-sm">
+              <Gauge value={Guninfo.weaponStats.reloadTimeSeconds} max={5} reverse label="reloadTimeSecondsーリロード速度" />
+            </div>
+            <div className=" backdrop-blur-sm">
+              <Gauge value={Guninfo.weaponStats.firstBulletAccuracy} max={5} label="firstBulletAccuracyー集団率" />
+            </div>
+            <div className=" backdrop-blur-sm">
+              <Gauge value={Guninfo.weaponStats.fireRate} max={16} label="fireRateー発射レート" />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
--- src/components/GUN LAYOUT/gauge.tsx
+++ src/components/GUN LAYOUT/gauge.tsx
@@ -10,12 +10,12 @@ export const Gauge = ({label,value,max,reverse = false }:GaugeProps) => {
   ? Math.min(100 - (value/max) * 100, 100)
   : Math.min((value/max)* 100, 100)
   return(
-    <div className="mb-2 border-r-4 p-10">
-      <div className="flex justify-between text-3xl mb-1">
-        <span className="text-center mb-5">{label}</span>
+    <div className="mb-2 border-r-4 p-10 w-full">
+      <div className="flex flex-col text-2xl mb-1">
+        <span className="text-center mb-5 break-words min-w-0">{label}</span>
         <span>{value}</span>
       </div>
-      <div className="w-[400px] bg-gray-700 rounded-full h-2">
+      <div className="w-full bg-gray-700 rounded-full h-2">
         <div
           className="bg-red-500 h-2 rounded-full"
           style={{ width: `${percent}%` }}
