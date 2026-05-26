2e3a67c1dc6e71be3f9dbf2db51397bd5b797ba1
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-27 00:06:53 +0900

    ページ遷移時のアニメーションの追加

---
+++ src/components/animation/PageTransition.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { motion, AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+export function PageTransition({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 0, y: 90 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.8, ease: "easeInOut" }}
+        className="flex-grow flex flex-col"
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
