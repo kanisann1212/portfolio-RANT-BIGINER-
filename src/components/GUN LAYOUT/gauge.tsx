e541d9f1d45d369a551e84dd582a77685cee8209
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-27 08:25:04 +0900

    銃レイアウトのアニメーション化

--- src/components/GUN LAYOUT/gauge.tsx
+++ src/components/GUN LAYOUT/gauge.tsx
@@ -1,24 +1,43 @@
+'use client'
+import { useEffect } from "react"
+import { motion, useTransform, animate, useMotionValue } from "framer-motion"
+
 type GaugeProps = {
-  label:string,
-  value:number,
-  max:number
-  reverse?:boolean,
+  label: string,
+  value: number,
+  max: number
+  reverse?: boolean,
 }
 
-export const Gauge = ({label,value,max,reverse = false }:GaugeProps) => {
+export const Gauge = ({ label, value, max, reverse = false }: GaugeProps) => {
+
+  const count = useMotionValue(0)
+  const rounded = useTransform
+
+  useEffect(() => {
+    const controls = animate(count, value, {
+      duration: 4,
+      ease: "easeOut",
+    })
+    return controls.stop
+  }, [])
+
   const percent = reverse
-  ? Math.min(100 - (value/max) * 100, 100)
-  : Math.min((value/max)* 100, 100)
-  return(
+    ? Math.min(100 - (value / max) * 100, 100)
+    : Math.min((value / max) * 100, 100)
+  return (
     <div className="mb-2 border-r-4 p-10 w-full">
       <div className="flex flex-col text-2xl mb-1">
         <span className="text-center mb-5 break-words min-w-0">{label}</span>
-        <span>{value}</span>
+        {/* <motion.span>{rounded}</motion.span> */}
       </div>
       <div className="w-full bg-gray-700 rounded-full h-2">
-        <div
+        <motion.div
+          initial={{ width: 0 }}
+          animate={{ width: `${percent}%` }}
+          transition={{ duration: 4, ease: "easeOut" }}
           className="bg-red-500 h-2 rounded-full"
-          style={{ width: `${percent}%` }}
+          style={{ originX: 0 }}
         />
       </div>
     </div>
