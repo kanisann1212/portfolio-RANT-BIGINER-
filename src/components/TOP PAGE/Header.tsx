5c1d970eaece6e01e1ec66fe553d7f06df7f8cf6
yuuma <obakasann1819@gmail.com>
2026-05-26 14:15:02 +0900

    ヘッダーの修正

--- src/components/TOP PAGE/Header.tsx
+++ src/components/TOP PAGE/Header.tsx
@@ -2,11 +2,15 @@ import Link from "next/link"
 import Image from "next/image"
 import { User } from "lucide-react"
 
-export const Header = () => {
+type WhiteProps = {
+  Hanten?:boolean
+}
+
+export const Header = ({Hanten}:WhiteProps) => {
   return (
-    <div className="flex justify-between items-center text-black  pt-5  border-b">
+    <div className={`flex justify-between items-center ${Hanten ? "text-white ":"text-black"}  pt-5  border-b`}>
       <div className="flex pl-10 pb-5 items-center">
-        <Link href="/" className="text-black">
+        <Link href="/" className = {`${Hanten ? "text-white ":"text-black"}`}>
           <Image
             src="/VALG.png"
             alt="VALOGO"
@@ -15,19 +19,16 @@ export const Header = () => {
           />
         </Link>
         <Link href="/">
-          <h1 className="text-black text-3xl pl-5 ">RANT BEGINNER</h1>
+          <h1 className={`${Hanten ? "text-white ":"text-black"} text-3xl pl-5 `}>RANT BEGINNER</h1>
         </Link>
       </div>
       <div className="space-x-15 flex pr-15 pb-5">
-        <Link href="/" className="text-black text-3xl ">HOME</Link>
-        <Link href="/guns" className="text-black text-3xl">WEAPON</Link>
-        <Link href="/map" className="text-black text-3xl">MAP</Link>
-        <Link href="/egents" className="text-black text-3xl">EGENT</Link>
+        <Link href="/" className={`${Hanten ? "text-white ":"text-black"} text-3xl `}>HOME</Link>
+        <Link href="/guns" className={`${Hanten ? "text-white ":"text-black"} text-3xl `}>WEAPON</Link>
+        <Link href="/map" className={`${Hanten ? "text-white ":"text-black"} text-3xl `}>MAP</Link>
+        <Link href="/egents" className={`${Hanten ? "text-white ":"text-black"} text-3xl `}>EGENT</Link>
         <User className="w-10 h-10 " />
       </div>
     </div>
   )
 }
-
-
-// 後で動画埋め込む
