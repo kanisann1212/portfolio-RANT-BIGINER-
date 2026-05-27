2426196d117d987a3262dcd24e557fdfa35e20ae
yuuma <obakasann1819@gmail.com>
2026-05-27 13:38:24 +0900

    トップページにアニメーション追加

--- src/components/TOP PAGE/Top.tsx
+++ src/components/TOP PAGE/Top.tsx
@@ -2,6 +2,7 @@ import { User } from "lucide-react"
 import { Video } from "@/components/TOP PAGE/Video";
 import { Midashi } from "@/components/MAP/Midashi";
 import Image from "next/image";
+import { MIBAE } from "./MIBAE";
 
 
 export const Top = () => {
@@ -23,29 +24,7 @@ export const Top = () => {
       <div className="fixed top-10 right-10" >
         <User className="w-10 h-10 text-white" />
       </div>
-
-
-      <div className="flex mt-96 mb-96  ml-24 mr-24 justify-between items-center" >
-        <div className="relative flex-shrink-0 w-3/5 h-[700px]">
-        <Image
-          src="/kai.jpg"
-          alt="VALORANT"
-          fill
-          className="object-cover rounded-3xl "
-        />
-        </div>
-        <div className="flex-1 pb-20 justify-between items-center">
-        <h1 className="text-black m-45 mb-20  text-7xl text-center">
-          ーFOR YOUー</h1>
-        <p className="text-black font-bold text-center p-10 text-2xl">このサイトは初心者である方全員に向けて作りました。<br />どうかこのサイトがあなたのバロ人生の糧になることを祈っております。</p>
-        </div>
-      </div>
-
-
-
-
-
-
+        <MIBAE syubun="このサイトは初心者である方全員に向けて作りました。" hukubun="どうかこのサイトがあなたのバロ人生の糧になることを祈っております。" kime="ーFOR YOUー" gazou="/kai.jpg"/>
       <div className="grid grid-cols-3 gap-10 m-15 mr-20 ml-20 h-[80vh]">
         <div className="h-full">
           <Midashi
