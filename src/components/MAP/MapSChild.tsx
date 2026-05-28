35f3c6fd7001ecc0727f5f0ccb779fc62ac91b6b
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-29 01:07:16 +0900

    マップページへアニメーション追加

--- src/components/MAP/MapSChild.tsx
+++ src/components/MAP/MapSChild.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import { MapType } from "@/types/type"
 import Image from "next/image"
 import { Header } from "../TOP PAGE/Header"
 import { MapsPattern } from "../../data/MapPattern"
 import { MapsRecomendparts } from "./MapsRecomend"
+import { motion } from "framer-motion"
 
-
+const variants = {
+  initial: { x: -2000, opacity: 0 },
+  animate: { x: 0, opacity: 1 },
+}
 
 export const MapSChild = ({ MapMicro }: { MapMicro: MapType }) => {
   const MapNameInfo: string = MapMicro.displayName
@@ -18,12 +24,26 @@ export const MapSChild = ({ MapMicro }: { MapMicro: MapType }) => {
           backgroundImage: `url(${MapMicro.stylizedBackgroundImage})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
-           minHeight: "100vh",
+          minHeight: "100vh",
+          minWidth: "1400px",
         }}
       >
         <Header Hanten={true} />
-        <h1 className="text-9xl">{MapMicro.displayName}</h1>
-        <div className="flex  items-center">
+        <motion.h1
+          className="text-9xl"
+          initial='initial'
+          whileInView="animate"
+          variants={variants}
+          viewport={{ once: true }}
+          transition={{
+            type: 'spring',
+            stiffness: 100,
+            damping: 10,
+            mass: 1,
+            duration: 1
+          }}
+        >{MapMicro.displayName}</motion.h1>
+        <div className="flex  items-center ">
           <div className="relative justify-between w-[1000] ">
             <Image
               src={MapMicro.displayIcon}
@@ -33,7 +53,7 @@ export const MapSChild = ({ MapMicro }: { MapMicro: MapType }) => {
               className=" w-full h-auto"
             />
           </div>
-           <MapsRecomendparts MapsRecomend={MapsRecomend} />
+          <MapsRecomendparts MapsRecomend={MapsRecomend} />
         </div>
       </div>
 
--- src/components/MAP/MapsRecomend.tsx
+++ src/components/MAP/MapsRecomend.tsx
@@ -1,12 +1,33 @@
+'use client'
 import Image from "next/image"
 import { Dela_Gothic_One } from "next/font/google"
 import type { MapAgent } from "@/types/type"
 import Link from "next/link"
+import { motion } from "framer-motion"
+
+const MotionLink = motion.create(Link)
 
 const valorantEgent = Dela_Gothic_One({
   weight: "400",
   subsets: ["latin"]
 })
+
+const textVariants = {
+  initial: { color: "#ffffff" },
+  hover: { color: "#ff0000" },
+  transition: {
+    duration: 1.5
+  }
+}
+
+
+const linkVariants = {
+  initial: { opacity: 0, y: 200 },
+  hover: { scale: 1.1 },
+  transition: {
+    duration: 1.5
+  }
+}
 
 type MapsRecomendProps = {
   MapsRecomend: MapAgent[]
@@ -15,13 +36,18 @@ const basepath = "/egents"
 
 export const MapsRecomendparts = ({ MapsRecomend }: MapsRecomendProps) => {
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-3 gap-10 shrink-0 min-w-[1400px]">
       {MapsRecomend.map((recomend) => {
         return (
-          <Link
-            className="flex flex-col items-center w-[450px] h-[450px]"
+          <MotionLink
+            className="flex flex-col items-center"
             key={recomend.uuid}
             href={`${basepath}/${recomend.uuid}`}
+            variants={linkVariants}
+            initial="initial"
+            whileHover="hover"
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
           >
             <div className="relative w-[450px] h-[450px]">
               <Image
@@ -38,8 +64,11 @@ export const MapsRecomendparts = ({ MapsRecomend }: MapsRecomendProps) => {
                 }}
               />
             </div>
-            <h1 className={`${valorantEgent.className} font-bold text-2xl`}>{recomend.name}</h1>
-          </Link>
+            <motion.h1
+              className={`${valorantEgent.className} font-bold text-2xl`}
+              variants={textVariants}
+            >{recomend.name}</motion.h1>
+          </MotionLink>
         )
       })}
     </div>
