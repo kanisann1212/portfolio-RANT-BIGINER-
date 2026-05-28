b768be804a13f9a8489a75dae1afce4852f8b397
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-29 01:08:25 +0900

    銃ページのコード整理

--- src/app/guns/page.tsx
+++ src/app/guns/page.tsx
@@ -2,14 +2,14 @@ import { AnimationGunlist } from "@/components/animation/AnimationGunlist"
 import { Header } from "@/components/TOP PAGE/Header"
 import { MIBAE } from "@/components/TOP PAGE/MIBAE"
 import type { WeponType, Skined } from "@/types/type"
-type Wepondata ={
-  data:WeponType[]
+type Wepondata = {
+  data: WeponType[]
 }
 
 export default async function Guns() {
 
   const res = await fetch("https://valorant-api.com/v1/weapons")
-  const json :Wepondata = await res.json()
+  const json: Wepondata = await res.json()
   const filterdWeponSkin: WeponType[] = json.data.map((wepon: WeponType) => ({
     ...wepon,
     skins: wepon.skins.filter((skin: Skined) => skin.chromas.length >= 4),
@@ -20,9 +20,7 @@ export default async function Guns() {
     <>
       <Header />
       <MIBAE gazou="/FIRE.jpg" syubun="エイムはすべての人に与えられた平等の力です。" hukubun="適切な武器と完璧なエイムで相手を叩き潰しましょう" kime="ーBring it onー" />
-     
       <AnimationGunlist filterdWeponSkin={filterdWeponSkin} />
-
     </>
   )
 }
