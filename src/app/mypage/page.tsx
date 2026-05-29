f1d4ac929df5dd3f3bf31a1b87b56ce286104102
CHIBA YUUMA <obakasann1819@gmail.com>
2026-05-29 21:09:05 +0900

    My pageの作成

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { DiscordLogoIcon,InstagramLogoIcon } from "@radix-ui/react-icons";
+
+export default function Mypage (){
+  return(
+    <div className=" container mx-auto flex flex-col items-center gap-3 justify-center h-screen w-screen">
+      <div className="font-extrabold text-5xl">Start your game</div>
+       <div>あなたのだけの物語を始めましょう</div>
+    <Button className="w-[100px] h-[50px] text-[20px]">Login</Button>
+     <div className="underline ">アカウントを持っていませんか？</div>
+    </div>
+  )
+}
