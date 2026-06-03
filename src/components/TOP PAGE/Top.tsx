92e97bb0ceb867bac3eb18867f8a8dc74cbb044d
CHIBA YUUMA <obakasann1819@gmail.com>
2026-06-04 08:22:25 +0900

    一部微修正

--- src/components/TOP PAGE/Top.tsx
+++ src/components/TOP PAGE/Top.tsx
@@ -1,11 +1,21 @@
+'use client'
 import { User } from "lucide-react"
 import { Video } from "@/components/TOP PAGE/Video";
 import { Midashi } from "@/components/TOP PAGE/Midashi";
 import Image from "next/image";
 import { MIBAE } from "./MIBAE";
+import { motion } from "framer-motion"
+import Link from "next/link";
 
+const MotionLink = motion.create(Link)
 
-
+  const linkVariants = {
+    initial: { opacity: 1, y: 0 },
+    hover: { scale: 1.5 },
+    transition: {
+      duration: 1.5
+    }
+  }
 
 export const Top = () => {
   return (
@@ -24,7 +34,14 @@ export const Top = () => {
         />
       </div>
       <div className="fixed top-10 right-10" >
+        <MotionLink
+          href="/mypage"
+          variants={linkVariants}
+          initial="initial"
+          whileHover="hover"
+        >
         <User className="w-10 h-10 text-white" />
+        </MotionLink>
       </div>
         <MIBAE syubun="このサイトは初心者である方全員に向けて作りました。" hukubun="どうかこのサイトがあなたのバロ人生の糧になることを祈っております。" kime="ーFOR YOUー" gazou="/kai.jpg"/>
       <div className="grid grid-cols-3 gap-10 m-15 mr-20 ml-20 h-[80vh]">
