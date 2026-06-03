42d0f4c5719974eee9fed2b53c85d37444226169
CHIBA YUUMA <obakasann1819@gmail.com>
2026-06-04 01:18:03 +0900

    バックエンドの実装　初期

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -6,7 +6,8 @@ export default function Mypage (){
     <div className=" container mx-auto flex flex-col items-center gap-3 justify-center h-screen w-screen">
       <div className="font-extrabold text-5xl">Start your game</div>
        <div>あなたのだけの物語を始めましょう</div>
-    <Button className="w-[100px] h-[50px] text-[20px]">Login</Button>
+    <Button className="w-[100px] h-[50px] text-[20px]"> Discord</Button>
+     <Button className="w-[100px] h-[50px] text-[20px]">google</Button>
      <div className="underline ">アカウントを持っていませんか？</div>
     </div>
   )
