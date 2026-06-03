747c5156e950cfa3c0903f6d99e7f515231cd412
CHIBA YUUMA <obakasann1819@gmail.com>
2026-06-04 01:17:42 +0900

    必要なデータのダウンロード

---
+++ src/auth.ts
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+import Discord from "next-auth/providers/discord"
+
+export const {handlers,auth,signIn,signOut} =NextAuth ({
+  providers:[
+    Discord({
+      clientId: process.env.AUTH_DISCORD_ID as string,
+      clientSecret: process.env.AUTH_DISCORD_SECRET as string
+    }),
+  ],
+  pages:{
+    signIn:"/login", 
+  },
+  session:{
+    strategy:"jwt"
+  },
+  callbacks:{
+    jwt({user,token}){
+      if(user){
+        token.id = user.id
+      }
+      return token
+    },
+    session({session,token}){
+      session.user.id = token.id 
+      return session
+    },
+  },
+})
