c1fe01ef365dd05cd7c3668d95d9f1c15f76754e
yuuma <obakasann1819@gmail.com>
2026-06-04 13:42:42 +0900

    proxyに名前を変更しファイルの場所を変更

---
+++ src/proxy.ts
@@ -0,0 +1,18 @@
+import { auth } from "@/auth"
+import { NextResponse } from "next/server"
+
+export const proxy = auth((req)=>{
+  const isLogIn = !!req.auth
+  const isLoginPage = req.nextUrl.pathname === "/login"
+
+  if (isLoginPage && isLogIn){
+    return NextResponse.redirect(new URL("/mypage",req.nextUrl.origin))
+  }
+  if(!isLogIn && req.nextUrl.pathname.startsWith("/mypage")){
+    return NextResponse.redirect(new URL("/login",req.nextUrl.origin))
+  }
+})
+
+export const config = {
+  matcher: ["/mypage", "/mypage/:path*","/login"],
+}
