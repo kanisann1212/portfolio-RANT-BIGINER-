d3fd199d1bd28704522b84a9fa0fe4c148ae7169
yuuma <obakasann1819@gmail.com>
2026-06-09 14:01:06 +0900

    投稿フォームの微修正

--- src/components/session/CreateVideo.tsx
+++ src/components/session/CreateVideo.tsx
@@ -5,12 +5,16 @@ import {
   Drawer,
   DrawerClose,
   DrawerContent,
+  DrawerDescription,
   DrawerFooter,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer"
 import { SelectAgent } from "@/components/session/SelectAgent";
+import { SelectMap } from "./SlectMap";
+import { toast } from "sonner"
+
 
 export type VideoFormData = {
   title: string
@@ -22,12 +26,17 @@ export type VideoFormData = {
 export const CreateVideo = () => {
   const { register, control, handleSubmit } = useForm<VideoFormData>()
 
-  const onSubmit = async (data:VideoFormData) => {
-    await fetch("/api/video", {
+  const onSubmit = async (data: VideoFormData) => {
+    const response = await fetch("/api/video", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     })
+    if(!response.ok){
+    toast.error("投稿に失敗しました。値を入力してやり直してください")
+    return
+    }
+
   }
 
   return (
@@ -38,31 +47,36 @@ export const CreateVideo = () => {
         >Open
         </DrawerTrigger>
       </div>
-      <DrawerContent
-        className="h-[700px] ">
-        <DrawerHeader>
-          <DrawerTitle className="text-2xl font-bold">CREATE YOUR VIDEO</DrawerTitle>
-          <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <DrawerContent
+          className="h-[700px] ">
+          <DrawerHeader>
+            <DrawerTitle className="text-2xl font-bold">CREATE YOUR VIDEO</DrawerTitle>
             <textarea
               id="title"
               autoFocus
               placeholder="Video Title"
+              {...register("title")}
               className="w-full resize-none overflow-hidden bg-transparent text-4xl focus:outline-none font-extrabold" />
             <textarea
               id="YoutubeURL"
               autoFocus
               placeholder="URL"
+              {...register("url")}
               className="w-full resize-none overflow-hidden bg-transparent text-4xl focus:outline-none font-extrabold" />
-          </form>
-        </DrawerHeader>
-        <DrawerFooter>
-          <SelectAgent control={control}/>
-          <Button>Submit</Button>
-          <DrawerClose>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
+          </DrawerHeader>
+          <div className="flex justify-between ml-5 mr-5">
+          <SelectAgent control={control} />
+          <SelectMap control={control} />
+          </div>
+          <DrawerFooter>
+            <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </div>
     </Drawer>
 
   )
