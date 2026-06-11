36145c2793dbb6bcf98e43fa2c7ebfb9c2d14bc6
yuuma <obakasann1819@gmail.com>
2026-06-11 13:57:46 +0900

    マイページランク表示部分の作成

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,102 +1,78 @@
-  import { auth } from "../../auth"
-  import Image from "next/image"
-  import { Header } from "@/components/TOP PAGE/Header";
-  import { CreateVideo } from "@/components/session/CreateVideo";
-  import { prisma } from "@/lib/prisma"
-  import { RANKjson } from "@/types/type";
-  import { KAISETSU } from "@/components/TOP PAGE/KAISETSU";
-  import { DBsumnail } from "@/components/session/DBsumnail";
-  import type { Agent } from "@/types/type"
-  type RANKdata = {
-    data: RANKjson
-  }
+import { auth } from "../../auth"
+import Image from "next/image"
+import { Header } from "@/components/TOP PAGE/Header";
+import { CreateVideo } from "@/components/session/CreateVideo";
+import { prisma } from "@/lib/prisma"
+import { KAISETSU } from "@/components/TOP PAGE/KAISETSU";
+import { DBsumnail } from "@/components/session/DBsumnail";
+import { RankHeader } from "@/data/RANK";
+import { IconHeader } from "@/data/Icon";
+import { Gauge } from "@/components/GUN LAYOUT/gauge";
 
-  const getRank = (count: number) => {
-    if (count >= 100) return 27
-    if (count >= 90) return 26
-    if (count >= 80) return 23
-    if (count >= 60) return 20
-    if (count >= 40) return 17
-    if (count >= 30) return 14
-    if (count >= 15) return 11
-    if (count >= 10) return 8
-    return 5
-  }
+export default async function Mypage() {
+  const session = await auth()
+  const EgentIcon1 = await IconHeader()
+  const VTier = await RankHeader()
+  const Video = await prisma.video.findMany({
+    where: { userId: session?.user.id },
+  })
 
-  type Egentdata = {
-    data: Agent[]
-  }
-
-  export default async function Mypage() {
-    const session = await auth()
-    const resIcon = await fetch("https://valorant-api.com/v1/agents?isPlayableCharacter=true&language=ja-JP")
-    const jsonIcon: Egentdata = await resIcon.json()
-    const EgentIcoN =session?.user.favoriteAgent
-    const EgentIcon1 =jsonIcon.data.find((a) => a.displayName === EgentIcoN)?.displayIcon ?? ""
-    console.log(EgentIcon1)
-  
-    const res = await fetch("https://valorant-api.com/v1/competitivetiers")
-    const json: RANKdata = await res.json()
-    const RANKD = json.data
-    const RANK = {
-      ...RANKD[4],
-      tiers: RANKD[4].tiers.filter((RAN) =>
-        RAN.tierName.includes("3") || RAN.tierName.includes("RADIANT")
-      ),
-    }
-
-    const Video = await prisma.video.findMany({
-      where: { userId: session?.user.id },
-    })
-    const VideoRank: number = Video.length
-    const VRank: number = getRank(VideoRank)
-    const VTier = RANK.tiers.find((t) => t.tier === VRank)
-
-    return (
-      <div>
-        <Header Hanten />
-        <div
-          style={{ backgroundColor: `#${VTier?.color}` }}
-          className="flex justify-between  items-center p-20 mask-b-from-90% mask-t-from-90%">
-          <div className="flex items-center gap-5">
+  return (
+    <div>
+      <Header />
+      <div
+        className="flex  justify-between bg-[url(/Reaver.jpg)] bg-left items-center p-20 mask-b-from-90% mask-t-from-90% bg-cover  h-[300px]">
+        <div className="flex items-center gap-5 ">
+          <Image
+            src={(session?.user.favoriteAgent ? EgentIcon1 : session?.user.image) ?? ""}
+            alt="VALORANT"
+            width={150}
+            height={150}
+            className="object-cover rounded-full "
+          />
+          <h1
+            className="text-9xl drop-shadow-[0_4px_12px_rgba(0,0,0,0.9)]  font-extrabold"
+            style={{
+              WebkitTextStroke: "4px rgba(0,0,0,0.85)",
+              paintOrder: "stroke fill",
+            }}
+          >{session?.user.name}</h1>
+        </div>
+        <div className="flex ">
+          {VTier && (
             <Image
-              src={(session?.user.favoriteAgent?EgentIcon1:session?.user.image)??""}
-              alt="VALORANT"
+              src={VTier.largeIcon}
+              alt={VTier.tierName}
               width={150}
               height={150}
-              className="object-cover rounded-full "
             />
-            <h1 className="text-8xl font-extrabold">{session?.user.name}</h1>
-          </div>
-          <div className="flex ">
-            {VTier && (
-              <Image
-                src={VTier.largeIcon}
-                alt={VTier.tierName}
-                width={150}
-                height={150}
-              />
-            )}
-            <h1 className="text-9xl font-extrabold text-white ">{VTier?.divisionName}</h1>
-          </div>
+          )}
+          <h1 className="text-9xl drop-shadow-[0_4px_12px_rgba(0,0,0,0.9)]  font-extrabold"
+            style={{
+              WebkitTextStroke: "4px rgba(0,0,0,0.85)",
+              paintOrder: "stroke fill",
+            }}>{VTier?.divisionName}</h1>
         </div>
-        <KAISETSU kime="TAKE YOUR VIDEO" syubun="あなたのランクを上げましょう" hukubun="マイページでは動画の投稿・管理が可能です。また動画投稿数に応じてあなたの表示ランクが上がります。たくさんあげて報酬をゲットしましょう" gazou="/RANK UP!!.png" />
+      </div>
+      <KAISETSU kime="TAKE YOUR VIDEO" syubun="あなたのランクを上げましょう" hukubun="マイページでは動画の投稿・管理が可能です。また動画投稿数に応じてあなたの表示ランクが上がります。たくさんあげて報酬をゲットしましょう" gazou="/RANK UP!!.png" />
+      <div className="flex ml-5">
         <CreateVideo />
-        {Video.length > 0  ? (<div className="grid lg:grid-cols-5 gap-2 grid-cols-2">
-          {Video.map((v) => {
-            return (
-              <div className=" w-[500px] h-[400px]" key={v.id}>
-                <div className="flex flex-col items-center justify-ceter gap-2 ">
-                  <DBsumnail url={v.url} EgentIcoN={v.agent} title={v.title} urlid={v.id}/>
-                  <div className="flex items-center gap-4 w-full">
-                    <div className="border-t border-black flex-1" />
-                  </div>
-                  <h1 className=" text-3xl font-bold pb-10">{v.title}</h1>
+        <Gauge value={Video.length} max={100} label="YOUR VIDEO" />
+      </div>
+      {Video.length > 0 ? (<div className="grid lg:grid-cols-5 gap-2 grid-cols-2">
+        {Video.map((v) => {
+          return (
+            <div className=" w-[500px] h-[350px]" key={v.id}>
+              <div className="flex flex-col items-center justify-ceter gap-2 ">
+                <DBsumnail url={v.url} EgentIcoN={v.agent} title={v.title} urlid={v.id} />
+                <div className="flex items-center gap-4 w-full">
+                  <div className="border-t border-black flex-1" />
                 </div>
               </div>
-            )
-          })}
-        </div>) : (<h1 className="text-8xl font-extrabold text-center text-white">動画がありません</h1>)}
-      </div>
-    )
-  }
+            </div>
+          )
+        })}
+      </div>) : (<h1 className="text-8xl font-extrabold text-center text-white">動画がありません</h1>)}
+    </div>
+  )
+}
