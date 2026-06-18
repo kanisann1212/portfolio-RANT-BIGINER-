308043ac8b7270b33abc2052ae627111c3f13232
yuuma <obakasann1819@gmail.com>
2026-06-18 12:54:31 +0900

    スタイルの微修正

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,19 +14,21 @@ export default function Loginform() {
         <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen w-screen items-center text-black justify-center bg-[url(https://totnfaipgpkmgjvlcqee.supabase.co/storage/v1/object/public/RANTBIGINNER.IMAGE/AISOLOGIN.jpg)] bg-cover bg-center lg:bg-position-[160px]">
 
           
-          <div className="relative hidden lg:block">
-            <Video />
-            <h1 className="text-white absolute top-130 left-40 text-8xl">RANT BIGINNER</h1>
-            <div className="absolute left-120 top-170">
-              <Image
-                src="https://totnfaipgpkmgjvlcqee.supabase.co/storage/v1/object/public/RANTBIGINNER.IMAGE/VALORANT.png"
-                alt="VALORANT"
-                width={200}
-                height={400}
-                className="h-auto"
-              />
-            </div>
-          </div>
+<div className="relative hidden lg:block">
+  <Video />
+
+  
+  <div className="absolute top-130 left-40 flex flex-col items-center">
+    <h1 className="text-white text-6xl">RANT BIGINNER</h1>
+    <Image
+      src="https://totnfaipgpkmgjvlcqee.supabase.co/storage/v1/object/public/RANTBIGINNER.IMAGE/VALORANT.png"
+      alt="VALORANT"
+      width={200}
+      height={400}
+      className="h-auto"
+    />
+  </div>
+</div>
 
          
 
